f5c195c4818054d4b78d6ef03b80a3dc538976b1
EnolaZhu <enola0314@gmail.com>
2022-04-05 17:47:12 +0800

    Import map view

---
+++ TravelLive/Controller/Map/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  TravelLive
+//
+//  Created by Enola Zhu on 2022/4/5.
+//
+
+import UIKit
+import GoogleMaps
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
