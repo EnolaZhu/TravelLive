939a54553823d05a2639054d56390081cc4dd69e
EnolaZhu <enola0314@gmail.com>
2022-04-06 14:38:29 +0800

    Add login storyboard

---
+++ TravelLive/Controller/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  TravelLive
+//
+//  Created by Enola Zhu on 2022/4/6.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func editingTextfield(_ sender: UIButton) {
+        print("Did touch")
+    }
+}
+
