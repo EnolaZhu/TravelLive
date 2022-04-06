56d23cc2363d2ff489c90d966fdd7f784de69768
EnolaZhu <enola0314@gmail.com>
2022-04-06 15:15:38 +0800

    Add Auth view xib and viewcontroller

---
+++ TravelLive/Controller/Login/AuthView.swift
@@ -0,0 +1,41 @@
+//
+//  AuthViewController.swift
+//  TravelLive
+//
+//  Created by Enola Zhu on 2022/4/6.
+//
+
+import UIKit
+
+
+class AuthView: UIView, NibOwnerLoadable {
+    
+    @IBOutlet weak var authTitleLabel: UILabel!
+    @IBOutlet weak var loginWithAppleButton: UIButton!
+    @IBOutlet weak var loginWithNativeButton: UIButton!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    private func customInit() {
+        loadNibContent()
+    }
+    
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//    }
+    
+}
