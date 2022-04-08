48ff446a87e81d5562ff215580f48e4d187b2d12
EnolaZhu <enola0314@gmail.com>
2022-04-09 01:48:13 +0800

    Add UILabel extension

---
+++ TravelLive/Extension/UILabel+Extension.swift
@@ -0,0 +1,84 @@
+//
+//  UILabel+Extension.swift
+//  TravelLive
+//
+//  Created by Enola Zhu on 2022/4/9.
+//
+
+import UIKit
+
+@IBDesignable
+extension UILabel {
+    
+    @IBInspectable var characterSpacing: CGFloat {
+        
+        set {
+            
+            if let labelText = text, labelText.count > 0 {
+                
+                let attributedString = NSMutableAttributedString(attributedString: attributedText!)
+                
+                attributedString.addAttribute(
+                    NSAttributedString.Key.kern,
+                    value: newValue,
+                    range: NSRange(location: 0, length: attributedString.length - 1)
+                )
+                
+                attributedText = attributedString
+            }
+        }
+        
+        get {
+            // swiftlint:disable force_cast
+            return attributedText?.value(forKey: NSAttributedString.Key.kern.rawValue) as! CGFloat
+            // swiftlint:enable force_cast
+        }
+        
+    }
+}
+@IBDesignable
+class EdgeInsetLabel: UILabel {
+    var textInsets = UIEdgeInsets.zero {
+        didSet { invalidateIntrinsicContentSize() }
+    }
+    
+    override func textRect(forBounds bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let insetRect = bounds.inset(by: textInsets)
+        let textRect = super.textRect(forBounds: insetRect, limitedToNumberOfLines: numberOfLines)
+        let invertedInsets = UIEdgeInsets(top: -textInsets.top,
+                                          left: -textInsets.left,
+                                          bottom: -textInsets.bottom,
+                                          right: -textInsets.right)
+        return textRect.inset(by: invertedInsets)
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: textInsets))
+    }
+}
+
+extension EdgeInsetLabel {
+    @IBInspectable
+    var leftTextInset: CGFloat {
+        set { textInsets.left = newValue }
+        get { return textInsets.left }
+    }
+    
+    @IBInspectable
+    var rightTextInset: CGFloat {
+        set { textInsets.right = newValue }
+        get { return textInsets.right }
+    }
+    
+    @IBInspectable
+    var topTextInset: CGFloat {
+        set { textInsets.top = newValue }
+        get { return textInsets.top }
+    }
+    
+    @IBInspectable
+    var bottomTextInset: CGFloat {
+        set { textInsets.bottom = newValue }
+        get { return textInsets.bottom }
+    }
+}
