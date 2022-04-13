b8516c67448d1b9ca1239b67de3f8e545b3e6476
EnolaZhu <enola0314@gmail.com>
2022-04-14 00:43:46 +0800

    Change pull view SDK

--- TravelLive/Controller/PullStreaming/PullStreamingViewController.swift
+++ TravelLive/Controller/PullStreaming/PullStreamingViewController.swift
@@ -1,59 +1,37 @@
 //
 //  PullStreamingViewController.swift
 //  TravelLive
-//
+//  https://cloud.tencent.com/document/product/454/56588
 //  Created by Enola Zhu on 2022/4/11.
 //
 
 import UIKit
-import IJKMediaFramework
+import TXLiteAVSDK_Professional
 
-class PullStreamingViewController: UIViewController {
-    var player: IJKFFMoviePlayerController!
+class PullStreamingViewController: UIViewController, V2TXLivePlayerObserver {
     private let loveButton = UIButton()
     var streamingUrl = String()
+    private var livePlayer = V2TXLivePlayer()
+    private var streamId: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        createPlayer()
+        startPlay()
         addChatView()
         createAnimationButton()
         loveButton.addTarget(self, action: #selector(click), for: .touchUpInside)
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        // 開始播放
-        player.prepareToPlay()
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        // 關閉播放器
-        player.shutdown()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    // "http://cdn-live1.qvc.jp/iPhone/800/800.m3u8"
-    func createPlayer() {
-        streamingUrl = "http://cdn-live1.qvc.jp/iPhone/800/800.m3u8"
-        let options = IJKFFOptions.byDefault()
-        let url = NSURL(string: streamingUrl)
-        let player = IJKFFMoviePlayerController(contentURL: url as URL?, with: options)
-        // 播放頁面寬高自適應
-        let autoresize = UIView.AutoresizingMask.flexibleWidth.rawValue | UIView.AutoresizingMask.flexibleHeight.rawValue
-        player?.view.autoresizingMask = UIView.AutoresizingMask(rawValue: autoresize)
-        player?.view.frame = self.view.bounds
-        // 縮放模式
-        player?.scalingMode = .aspectFit
-        // 啟動後自動播放
-        player?.shouldAutoplay = true
-        
-        view.autoresizesSubviews = true
-        view.addSubview((player?.view)!)
-        self.player = player
+    func startPlay() {
+        livePlayer.setRenderView(view)
+        // "http://cdn-live1.qvc.jp/iPhone/800/800.m3u8"
+        let url = "webrtc://pull.whiletrue.cafe/TravelLive/Broccoli2"
+        livePlayer.startPlay(url)
     }
     
     private func addChatView() {
