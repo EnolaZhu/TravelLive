e74c7a0ec1deb4194e271e1f19078996b3ebb7b7
EnolaZhu <enola0314@gmail.com>
2022-04-18 00:25:42 +0800

    Create SearchCollectionView

--- TravelLive/Controller/Search/SearchViewController.swift
+++ TravelLive/Controller/Search/SearchViewController.swift
@@ -7,6 +7,68 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UICollectionViewDataSource, GridLayoutDelegate {
+    var images = [UIImage]()
+    @IBOutlet weak var searchCollectionView: UICollectionView!
+    @IBOutlet weak var gridLayout: GridLayout!
     
+    var arrInstaBigCells = [Int]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        images = Array(repeatElement(#imageLiteral(resourceName: "avatar"), count: 99))
+        arrInstaBigCells.append(1)
+        
+        var tempStorage = false
+        for _ in 1...21 {
+            if(tempStorage){
+                arrInstaBigCells.append(arrInstaBigCells.last! + 10)
+            } else {
+                arrInstaBigCells.append(arrInstaBigCells.last! + 8)
+            }
+            tempStorage = !tempStorage
+        }
+        
+        print(arrInstaBigCells)
+        searchCollectionView.backgroundColor = .white
+        searchCollectionView.dataSource = self
+        searchCollectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        searchCollectionView.contentOffset = CGPoint(x: -10, y: -10)
+        
+        gridLayout.delegate = self
+        gridLayout.itemSpacing = 3
+        gridLayout.fixedDivisionCount = 3
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+    // MARK: - UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 99
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCollectionViewCell", for: indexPath) as? SearchCollectionViewCell else { return UICollectionViewCell() }
+        cell.imageView.image = images[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - PrimeGridDelegate
+    
+    func scaleForItem(inCollectionView collectionView: UICollectionView, withLayout layout: UICollectionViewLayout, atIndexPath indexPath: IndexPath) -> UInt {
+        if(arrInstaBigCells.contains(indexPath.row) || (indexPath.row == 1)){
+            return 2
+        } else {
+            return 1
+        }
+    }
+    
+    func itemFlexibleDimension(inCollectionView collectionView: UICollectionView, withLayout layout: UICollectionViewLayout, fixedDimension: CGFloat) -> CGFloat {
+        return fixedDimension
+    }
 }
