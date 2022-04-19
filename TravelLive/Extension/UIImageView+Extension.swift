d26add8cabbdba7c43bc076338931154f1a1ee1d
EnolaZhu <enola0314@gmail.com>
2022-04-19 21:40:51 +0800

    Add UIImage extension

---
+++ TravelLive/Extension/UIImageView+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIImageView+Extension.swift
+//  TravelLive
+//
+//  Created by Enola Zhu on 2022/4/19.
+//
+
+import UIKit
+
+extension UIImageView {
+
+    func makeRounded() {
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = false
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+    }
+}
