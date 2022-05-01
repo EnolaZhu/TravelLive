c9ed948c16e55ee1501cc826e1c09b5293b5dcc9
EnolaZhu <enola0314@gmail.com>
2022-05-01 10:11:23 +0800

    Add extension

---
+++ TravelLive/Extension/String.Index+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  String.Index+Extension.swift
+//  TravelLive
+//
+//  Created by Enola Zhu on 2022/5/1.
+//
+
+import Foundation
---
+++ TravelLive/Extension/StringProtocol+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  StringProtocol+Extension.swift
+//  TravelLive
+//
+//  Created by Enola Zhu on 2022/5/1.
+//
+
+import Foundation
