fb93681b128db7ff202375a47588a26f49cbccfd
EnolaZhu <enola0314@gmail.com>
2022-05-10 15:28:09 +0800

    Modify layout problem

--- TravelLive/Extension/UIButton+Extension.swift
+++ TravelLive/Extension/UIButton+Extension.swift
@@ -9,15 +9,28 @@ import UIKit
 
 extension UIButton {
     // swiftlint:disable opening_brace line_length function_parameter_count
+    @available(iOS 15.0, *)
     func configureButton(text: String, image: UIImage, imagePadding: CGFloat, edgePadding: CGFloat, button: UIButton, backgroundColor: UIColor, textColor: UIColor)  {
-        var configuration = UIButton.Configuration.filled()
-        configuration.title = text
-        configuration.image = image
-        configuration.imagePadding = imagePadding
-        configuration.contentInsets = NSDirectionalEdgeInsets(top: edgePadding, leading: edgePadding, bottom: edgePadding, trailing: edgePadding)
-        configuration.background.backgroundColor = backgroundColor
-        configuration.baseForegroundColor = textColor
-        button.configuration = configuration
+        if #available(iOS 15.0, *) {
+            var configuration = UIButton.Configuration.filled()
+        } else {
+            // Fallback on earlier versions
+        }
+        configuration?.title = text
+        configuration?.image = image
+        configuration?.imagePadding = imagePadding
+        if #available(iOS 15.0, *) {
+            configuration?.contentInsets = NSDirectionalEdgeInsets(top: edgePadding, leading: edgePadding, bottom: edgePadding, trailing: edgePadding)
+        } else {
+            // Fallback on earlier versions
+        }
+//        configuration.background.view.backgroundColor = backgroundColor
+        configuration?.baseForegroundColor = textColor
+        if #available(iOS 15.0, *) {
+            button.configuration = configuration
+        } else {
+            // Fallback on earlier versions
+        }
     }
     
     func hasImage(named imageName: String, for state: UIControl.State) -> Bool {
